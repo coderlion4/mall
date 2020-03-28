2fa4a249a3dee800813203a2adfc35709dc2791d
SpeakSoftly <speaksoftly@SpeakSofylt.local>
2020-03-28 22:14:32 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from "./store"
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from "./store";
 
-import VueLazyLoad from "vue-lazyload"
+import VueLazyLoad from "vue-lazyload";
 
-import FastClick from "fastclick"
+import FastClick from "fastclick";
 
-import toast from "components/common/toast"
+import toast from "components/common/toast";
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(toast);
 
