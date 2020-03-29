2880678c320e6f2255db1dc312f1fc09719badf1
SpeakSoftly <speaksoftly@SpeakSofylt.local>
2020-03-30 01:26:06 +0800

    基本搭建和首页开发

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,8 @@ module.exports = {
       viewportUnit: "vw",   // 指定需要转换成的视口单位, 建议使用vw
       selectorBlackList: ["ignore"],   // 指定不需要转换的类
       minPixelValue: 1,     // 小于或等于'1px'不转换为视口单位
-      mediaQuery: false     // 允许在媒体查询中转换'px'
+      mediaQuery: false,    // 允许在媒体查询中转换'px'
+      exclude: [/TabMenu\.vue/]
     }
   }
 }
